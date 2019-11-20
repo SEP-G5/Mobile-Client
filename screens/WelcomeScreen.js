c700c0c544dc283ce882b74c500c205d44fc9a1f
Brent Heftye <bheftye92@gmail.com>
2019-11-20 14:42:13 +0100

    Custom Font Roboto.

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { View } from 'react-native';
-import SimpleComponent from '../components/SimpleComponent'
+import SimpleComponent from '../components/SimpleComponent';
+import * as Font from 'expo-font';
 
-export default function HomeScreen() {
+export default class HomeScreen extends Component {
+  componentDidMount() {
+    Font.loadAsync({
+      'roboto': require('./../assets/fonts/Roboto-Regular.ttf'),
+    });
+  }
+
+  render(){
     return (
-        <View>
-            <SimpleComponent />
-        </View>
+      <View>
+        <SimpleComponent />
+      </View>
     );
+  }
+
 }
