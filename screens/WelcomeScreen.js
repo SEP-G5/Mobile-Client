51c90e8c1dd7f60c3efe4046e26e8174f7dd71d2
Brent Heftye <bheftye92@gmail.com>
2019-11-25 11:40:39 +0100

    Removed Roboto from fonts.

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -4,12 +4,7 @@ import SimpleComponent from '../components/SimpleComponent';
 import * as Font from 'expo-font';
 
 export default class HomeScreen extends Component {
-  componentDidMount() {
-    Font.loadAsync({
-      'roboto': require('./../assets/fonts/Roboto-Regular.ttf'),
-    });
-  }
-
+  
   render(){
     return (
       <View>
