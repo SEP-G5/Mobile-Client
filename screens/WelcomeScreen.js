06dbe85dffa8cdefe54ddc1baacfcbe4673f41df
Brent Heftye <bheftye92@gmail.com>
2019-11-25 11:41:15 +0100

    Continuous removal of Roboto.

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,10 +1,9 @@
 import React, {Component} from 'react';
 import { View } from 'react-native';
 import SimpleComponent from '../components/SimpleComponent';
-import * as Font from 'expo-font';
 
 export default class HomeScreen extends Component {
-  
+
   render(){
     return (
       <View>
