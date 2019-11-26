d56a9d62d51e6a8808b2324554f44acdff4c12b9
Brent Heftye <bheftye92@gmail.com>
2019-11-26 18:09:37 +0100

    Camera Button Added to the Welcome Screen.

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,10 +1,15 @@
 import React, {Component} from 'react';
 import { View } from 'react-native';
 import SimpleComponent from '../components/SimpleComponent';
+import TopBarIcon from '../components/TopBarIcon';
+import { Platform, TouchableOpacity } from 'react-native';
 
 export default class HomeScreen extends Component {
   static navigationOptions = {
     title: 'Home',
+    headerRight: <TouchableOpacity>
+      <TopBarIcon name={Platform.OS === 'ios'?`ios-camera`:'md-camera'}/>
+    </TouchableOpacity>
   };
 
   render(){
