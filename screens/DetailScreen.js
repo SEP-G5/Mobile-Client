d740f9207ec70057a95425f3093d3e77f5dd500e
Brent Heftye Sanchez <bheftye92@gmail.com>
2019-12-16 11:39:54 +0100

    Modified the navigation when transfering to scan a QR Code.

--- screens/DetailScreen.js
+++ screens/DetailScreen.js
@@ -6,13 +6,17 @@ class DetailScreen extends Component {
     render(){
         const item = this.props.navigation.getParam('item');
         return (
-            <View style={{flex:1, paddingTop:10, paddingLeft:10, paddingRight:10}}>
-                <Text style={{fontSize:16, fontStyle:'italic', paddingBottom:30}}>{item.id}</Text>
-                <Text style={{fontSize:14, paddingBottom:30}}>{item.name}</Text>
-                <Button
-                    title="Transfer Ownership"
-                    onPress={() => alert('Transfer')}
-                />
+            <View style={{flex:1, paddingLeft:10, paddingRight:10, justifyContent:'center'}}>
+                <View style={{flex:2}}>
+                    <Text style={{fontSize:16, paddingTop:20,paddingBottom:30}}>{item.name}</Text>
+                    <Text style={{fontSize:14, fontStyle:'italic', paddingBottom:30}}>SN: {item.id}</Text>
+                </View>
+                <View style={{flex:1}}>
+                    <Button
+                        title="Transfer Ownership"
+                        onPress={() => this.props.navigation.navigate('Scanner')}
+                    />
+                </View>
             </View>
         );
     }
