bf40f47c66b138609cd711c1566447c9ba7909e6
Brent Heftye Sanchez <bheftye92@gmail.com>
2019-12-17 11:56:07 +0100

    Send Transfer Ownership Transaction.

--- screens/SendTransactionScreen.js
+++ screens/SendTransactionScreen.js
@@ -1,8 +1,27 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
 import {View, Text, StyleSheet} from 'react-native';
 import {Button} from 'react-native-elements';
+import {createTransaction, sendTransaction} from "../actions/TransactionAction";
 
-export default class SendTransactionScreen extends Component{
+class SendTransactionScreen extends Component{
+
+    componentWillReceiveProps(nextProps, nextContext) {
+        const {transaction} = nextProps;
+        const {transaction: oldTransaction} = this.props;
+        if (transaction.timestamp !== oldTransaction.timestamp){
+            //If there is a new transaction to send... send it.
+            this.props.sendTransaction(transaction);
+        }
+    }
+
+    onPressConfirm = () => {
+        const {publicKey, privateKey, navigation} = this.props;
+        const publicKeyOut = navigation.getParam('data');
+        const item = navigation.getParam('item');
+        this.props.createTransaction(item.id, publicKey, publicKeyOut, privateKey);
+    };
+
     render(){
         const {navigation} = this.props;
         const item = navigation.getParam('item');
@@ -15,12 +34,20 @@ export default class SendTransactionScreen extends Component{
                 <Text style={styles.txt}>Destination: {data}</Text>
                 <Button
                     title={'Confirm Transaction'}
-                    onPress={() => alert('Ownership sent')}
+                    onPress={() => this.onPressConfirm()}
                 />
             </View>
         );
     }
 }
+
+const mapStateToProps = (state) => ({
+    publicKey: state.get('keyPair').get('publicKey'),
+    privateKey: state.get('keyPair').get('privateKey'),
+    transaction: state.get('transaction').get('transaction')
+});
+
+export default connect(mapStateToProps,{createTransaction, sendTransaction})(SendTransactionScreen)
 
 const styles = StyleSheet.create({
     txt: {fontSize:18, paddingBottom:10}
