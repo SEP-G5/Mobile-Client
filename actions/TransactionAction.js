a510271f2853629d28d56495401b7884d7287bb0
Brent Heftye Sanchez <bheftye92@gmail.com>
2020-01-15 02:31:18 +0100

    Transfer Ownership process with the new keys and process. There is a problem with the signature might need backend help.

--- actions/TransactionAction.js
+++ actions/TransactionAction.js
@@ -33,6 +33,8 @@ export const SET_VIEW_DETAIL = 'set_view_detail';
 export const SET_SN = 'set_sn';
 export const SET_NAME = 'set_name';
 export const RESET_REGISTER_BIKE_STATE = 'reset_register_bike_state';
+export const RESET_TRANSFER_OWNERSHIP_STATE = 'reset_transfer_ownership_state';
+
 
 export const createTransaction = (id, publicKeyInput, publicKeyOutput, privateKey) => {
     return (dispatch) => {
@@ -111,6 +113,8 @@ export const sendTransaction = (transaction) => {
                 'Content-Type': 'application/json'
             },
         };
+
+        console.log(transaction);
 
         Peer.sendRequest(SEND_TRANSACTION_URL, request).then(function (response) {
             //We need to process the response
@@ -389,5 +393,15 @@ export const setName = (value) => {
 export const resetRegisterBikeState = () => {
     return {
         type: RESET_REGISTER_BIKE_STATE
+    }
+};
+
+/**
+ * Reset the state that handles transfer of ownership.
+ * @returns {{type: string}}
+ */
+export const resetTransferOwnershipState = () => {
+    return {
+        type: RESET_TRANSFER_OWNERSHIP_STATE
     }
 };
--- screens/SendTransactionScreen.js
+++ screens/SendTransactionScreen.js
@@ -2,9 +2,12 @@ import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {View, Text, StyleSheet} from 'react-native';
 import {Button} from 'react-native-elements';
-import {createTransaction, sendTransaction} from "../actions/TransactionAction";
+import {createTransaction, sendTransaction, resetTransferOwnershipState} from "../actions/TransactionAction";
 
 class SendTransactionScreen extends Component{
+    static navigationOptions = {
+        title: 'Transfer Ownership'
+    };
 
     componentWillReceiveProps(nextProps, nextContext) {
         const {transaction} = nextProps;
@@ -17,25 +20,53 @@ class SendTransactionScreen extends Component{
 
     onPressConfirm = () => {
         const {publicKey, privateKey, navigation} = this.props;
-        const publicKeyOut = navigation.getParam('data');
-        const item = navigation.getParam('item');
-        this.props.createTransaction(item.id, publicKey, publicKeyOut, privateKey);
+        const publicKeyOut = JSON.parse(navigation.getParam('data'));
+        const id = navigation.getParam('item');
+        this.props.createTransaction(id, publicKey, publicKeyOut, privateKey);
     };
 
+    handleOnPressContinue = () => {
+        this.props.resetTransferOwnershipState();
+        this.props.navigation.navigate('Home');
+    };
+
+
     render(){
-        const {navigation} = this.props;
-        const item = navigation.getParam('item');
+        const {navigation, success, loading} = this.props;
+        const id = navigation.getParam('item');
         const data = navigation.getParam('data');
-        return (
-            <View style={{flex:1, paddingLeft:10, paddingRight:10}}>
-                <Text style={styles.txt}>Transaction Details</Text>
-                <Text style={styles.txt}>Type: Transfer Ownership</Text>
-                <Text style={styles.txt}>SN: {item.id}</Text>
-                <Text style={styles.txt}>Destination: {data}</Text>
+
+        if (loading) {
+            return <View style={styles.container}>
+                <Text style={{fontSize: 24, marginTop: 15, textAlign: 'center'}}>Loading...</Text>
+            </View>
+        }
+
+        if (success) {
+            return <View style={styles.container}>
+                <Text style={{fontSize: 24, marginTop: 15, textAlign: 'center'}}>Transaction Successful</Text>
                 <Button
-                    title={'Confirm Transaction'}
-                    onPress={() => this.onPressConfirm()}
+                    title={'Continue'}
+                    onPress={() => this.handleOnPressContinue()}
+                    style={{marginTop: 30, paddingLeft: 20, paddingRight: 20}}
                 />
+            </View>
+        }
+
+        return (
+            <View style={{flex:1, paddingLeft:15, paddingRight:15, marginTop:15}}>
+                <View style={{flex:2, justifyContent:'center'}}>
+                    <Text style={styles.header}>Transaction Details</Text>
+                    <Text style={styles.txt}># {id}</Text>
+                    <Text style={styles.txt}>Destination: {data}</Text>
+                </View>
+                <View style={{flex:1, justifyContent:'bottom'}}>
+                    <Button
+                        title={'Confirm Transaction'}
+                        onPress={() => this.onPressConfirm()}
+                        style={{marginTop: 50}}
+                    />
+                </View>
             </View>
         );
     }
@@ -44,11 +75,15 @@ class SendTransactionScreen extends Component{
 const mapStateToProps = (state) => ({
     publicKey: state.get('keyPair').get('publicKey'),
     privateKey: state.get('keyPair').get('privateKey'),
-    transaction: state.get('transaction').get('transaction')
+    transaction: state.get('transaction').get('transaction'),
+    success: state.get('transaction').get('success'),
+    loading: state.get('transaction').get('loading')
 });
 
-export default connect(mapStateToProps,{createTransaction, sendTransaction})(SendTransactionScreen)
+export default connect(mapStateToProps,{createTransaction, sendTransaction, resetTransferOwnershipState})(SendTransactionScreen)
 
 const styles = StyleSheet.create({
-    txt: {fontSize:18, paddingBottom:10}
+    container: {flex: 1, justifyContent: 'center', paddingBottom: 10, paddingTop: 10, paddingLeft: 5, paddingRight: 5},
+    header: {fontSize:24, paddingBottom: 25},
+    txt: {fontSize:20, paddingBottom:15, color:'#aaa'},
 });
